c7e723165e3d60f32cd7c392080fd3ec3a13a4e2
Philip Kreißel <22598920+pkreissel@users.noreply.github.com>
2026-04-03 18:32:00 +0200

    fix preview

--- preview/main.tsx
+++ preview/main.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FactCheckSearchEdit } from '../src/components/fact-check-search/edit';
+import { FactCheckBar } from './SearchComponent';
 import '../src/components/fact-check-search/style.scss';
 
 const app = document.querySelector('#app');
 
-const elements = {
-    styleComponents: () => null,
-};
-
-const attrs = {
-    searchApiUrl: { desktop: { value: '' } },
-    importApiUrl: { desktop: { value: '' } },
-};
-
 if (app) {
     ReactDOM.render(
-        <FactCheckSearchEdit
-            attrs={attrs as any}
-            elements={elements as any}
-            id="preview-fact-check-search"
-            name="vvp/fact-check-search"
-        />,
+        <FactCheckBar />,
         app
     );
 }
