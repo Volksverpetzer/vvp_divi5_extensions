537a4d3a8445588cf8f3654b158a96e53bc1511c
veeck <gitkraken@veeck.de>
2026-04-21 09:47:40 +0200

    Add eslitn and prettier, remove tailwind

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "node_modules/**",
+      "vendor/**",
+      "modules-json/**",
+      "scripts/**",
+      "styles/**",
+      ".cache/**",
+      "dist/**",
+      "build/**",
+    ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  ...compat.extends("eslint:recommended"),
+  ...compat.extends("plugin:@typescript-eslint/recommended").map((config) => ({
+    ...config,
+    files: ["**/*.{ts,tsx}"],
+  })),
+  ...compat
+    .extends("plugin:react/recommended", "plugin:react-hooks/recommended")
+    .map((config) => ({
+      ...config,
+      files: ["**/*.{tsx,jsx}"],
+    })),
+  ...compat.extends("prettier"),
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-empty": ["error", { allowEmptyCatch: true }],
+    },
+  },
+  {
+    files: ["**/*.{tsx,jsx}"],
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
